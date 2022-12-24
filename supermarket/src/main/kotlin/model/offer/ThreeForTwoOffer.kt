396e0f071d9a425600667df7a6b1fe47123cee55
David <santamadavid@gmail.com>
2022-12-24 13:56:31 +0100

    18 - minor refactors

--- supermarket/src/main/kotlin/model/offer/ThreeForTwoOffer.kt
+++ supermarket/src/main/kotlin/model/offer/ThreeForTwoOffer.kt
@@ -5,7 +5,11 @@ import model.product.SingleProduct
 
 class ThreeForTwoOffer(
     product: SingleProduct,
-): ItemsForAmountOffer(product, 3, product.unitPrice * 2) {
+): ItemsForAmountOffer(
+    product = product,
+    items = 3,
+    amount = product.unitPrice * 2
+) {
 
     override fun shouldDiscountBeApplied(productQuantities: List<ProductQuantity>): Boolean =
         (findProductQuantity(productQuantities)?.quantity?.toInt() ?: 0) > 2
